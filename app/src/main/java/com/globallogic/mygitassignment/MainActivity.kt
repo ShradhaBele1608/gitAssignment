fd295d6fd96fbda36ecb9e86bb6461c3a70e8477
Shradha_Bele <67739776+ShradhaBele1608@users.noreply.github.com>
2020-07-02 23:03:16 +0530

    branch1 conflict resolve

--- app/src/main/java/com/globallogic/mygitassignment/MainActivity.kt
+++ app/src/main/java/com/globallogic/mygitassignment/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     val MY_CONST = 4
     val MY_CONST2 = 43
     override fun onCreate(savedInstanceState: Bundle?) {
