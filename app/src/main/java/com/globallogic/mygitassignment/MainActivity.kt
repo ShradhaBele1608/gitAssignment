d96cb57ca155a6b5626ddff7bcd226721fd510cd
Shradha_Bele <67739776+ShradhaBele1608@users.noreply.github.com>
2020-07-02 22:45:20 +0530

    branch1 commit

--- app/src/main/java/com/globallogic/mygitassignment/MainActivity.kt
+++ app/src/main/java/com/globallogic/mygitassignment/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.globallogic.mygitassignment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    val MY_CONST = 42
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
